7379415bed705ab97eee7af16683b52434d5efac
PriyaChouhan456 <124526485+PriyaChouhan456@users.noreply.github.com>
2023-02-11 13:30:00 +0530

    Add files via upload

---
+++ desktopnotifier.py
@@ -0,0 +1,11 @@
+from plyer import notification
+title="TAKE A BREAK AND DRINK WATER  "
+message="Keeping your body hydrated is crucial for health and well-being"
+        
+        
+notification.notify(title= title,
+                    message= message,
+                    app_icon= None,
+                    timeout= 40
+    
+    )
